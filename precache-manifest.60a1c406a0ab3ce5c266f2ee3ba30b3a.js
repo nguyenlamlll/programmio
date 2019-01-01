d1a4ef0ce2dfa72d284381d63e35555378d82074
Lam Le <15520405@gm.uit.edu.vn>
2019-01-01 15:26:34 +0700

    Updates

---
+++ precache-manifest.60a1c406a0ab3ce5c266f2ee3ba30b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/programmio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c62adabf1ce0322a79c1174220b450a8",
+    "url": "/programmio/static/media/fire.c62adabf.png"
+  },
+  {
+    "revision": "dbca039814c7a301e3ca",
+    "url": "/programmio/static/js/runtime~main.dbca0398.js"
+  },
+  {
+    "revision": "c10f13541ac32ddb631c",
+    "url": "/programmio/static/js/main.c10f1354.chunk.js"
+  },
+  {
+    "revision": "404807e4c413fe3e64ef",
+    "url": "/programmio/static/js/1.404807e4.chunk.js"
+  },
+  {
+    "revision": "c10f13541ac32ddb631c",
+    "url": "/programmio/static/css/main.4b0499b7.chunk.css"
+  },
+  {
+    "revision": "99b007e8201ec790d8da407760ceb6ab",
+    "url": "/programmio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/programmio/precache-manifest.7e9d36bf783efc94728b01923b38abf9.js"
+  "/programmio/precache-manifest.60a1c406a0ab3ce5c266f2ee3ba30b3a.js"
 );
 
 workbox.clientsClaim();
